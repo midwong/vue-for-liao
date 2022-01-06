a854421c00317bc3c35a7c1bfae8133e4d0ae177
hz <huangz@justsafe.com>
2022-01-06 14:35:50 +0800

    init

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: 'Vue for circle liao',
 
   /**
    * @type {boolean} true | false
--- src/views/homepage/index.vue
+++ src/views/homepage/index.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <h1>home works!</h1>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
+  name: 'HomePage',
   computed: {
-    ...mapGetters([
-      'name'
-    ])
   }
 }
 </script>
